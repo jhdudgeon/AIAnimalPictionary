0702fe89ec4cc661e26d727a39184b77049d7e28
James Dudgeon <jhdudgeon12@gmail.com>
2024-04-01 17:22:08 -0500

    Initial Commit

---
+++ AI Animal Pictionary/AI_Animal_PictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Animal_PictionaryApp.swift
+//  AI Animal Pictionary
+//
+//  Created by James Dudgeon on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct AI_Animal_PictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
